d0a42a06c0c044bbc9abde6a7e6f1a14948b31c4
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-31 15:27:39 -0300

    Melhorando espaçamento do Home mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
 
   return (
     <>
-      <section className="min-h-[60rem] pt-12">
-        <div className="min-h-[40rem] md:min-h-[27rem] flex flex-col justify-between">
+      <section className="md:min-h-[60rem] md:pt-12">
+        <div className="md:min-h-[27rem] flex flex-col justify-between">
           <p className='text-5xl md:text-7xl font-extrabold'>As mais recentes novidades e tecnologias diretamente no seu bolso.
           </p>
           <p className="text-xl text-gray-400 max-w-screen-md">Explore smartphones de última geração, acessórios modernos e gadgets inovadores. Descubra produtos exclusivos e ofertas especiais para encontrar a tecnologia perfeita para você</p>
