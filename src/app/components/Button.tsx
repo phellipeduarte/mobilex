03d32108105adb5bde7f2a7a3c0ccda07c5d6a29
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-30 22:34:42 -0300

    Alterando cor de detalhes

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -7,6 +7,6 @@ interface ButtonProps {
 
 export const Button = (props: ButtonProps) => {
     return (
-        <button type="button" className="transition text-black bg-white focus:text-white focus:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-900 font-medium rounded text-sm px-4 py-2 text-center hover:text-white hover:bg-green-600">{props.text}</button>
+        <button type="button" className="transition text-black bg-white focus:text-white focus:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-white font-medium rounded text-sm px-4 py-2 text-center hover:text-white hover:bg-gray-400">{props.text}</button>
     )
 }
