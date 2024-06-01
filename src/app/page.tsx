78ced6fe302eee5cccd07cb122206b3ba9473322
phellipeduarte <phellipe.duarte@icloud.com>
2024-06-01 13:08:57 -0300

    Alterando imagem para diferentes temas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { Button, ButtonSize, ButtonType } from './components/Button';
 
 export default function Home() {
 
+  const imageURL = "https://i.postimg.cc/zBgKqj5X/iphone-light.jpg"
+  const darkImageURL = "https://i.postimg.cc/N0pCxbZ0/iphone15.png"
+
   return (
     <>
-      <section className="md:min-h-[60rem] md:pt-12">
+      <section className="min-h-full dark:md:min-h-[60rem] md:pt-12">
         <div className="md:min-h-[27rem] flex flex-col justify-between">
           <p className='text-5xl md:text-7xl font-extrabold'>As mais recentes novidades e tecnologias diretamente no seu bolso.
           </p>
@@ -15,12 +18,20 @@ export default function Home() {
             <Button text="Saiba Mais" to="/marketplace" size={ButtonSize.lg} type={ButtonType.secondary} />
           </div>
         </div>
-        <div className="flex justify-end py-12 md:pt-24">
+        <div className="flex justify-end pt-auto dark:py-12 md:pt-24">
           <Image
-            src="https://i.postimg.cc/N0pCxbZ0/iphone15.png"
+            className="dark:hidden"
+            src={imageURL}
+            width={500}
+            height={150}
+            alt="iphone"
+          />
+          <Image
+            className="hidden dark:block"
+            src={darkImageURL}
             width={900}
             height={150}
-            alt="iphones"
+            alt="iphone"
           />
         </div>
       </section>
